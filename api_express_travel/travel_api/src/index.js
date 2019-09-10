cd7c1dd586791aea3afafd7191577520fb817452
P Lowe <perun@reagan.com>
2019-09-10 10:48:29 -0400

    update v4

--- api_express_travel/travel_api/src/index.js
+++ api_express_travel/travel_api/src/index.js
@@ -6,6 +6,14 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
